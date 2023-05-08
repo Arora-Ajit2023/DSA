0d9876c0f8ab32193a8e52fc1654f9ac5dd5f878
Arora-Ajit <aroraajit123@gmail.com>
2023-05-08 14:19:00 +0530

    added fibonacci solution

--- day6.js
+++ day6.js
@@ -20,3 +20,18 @@ const CheckIndex = (arr, value) => {
 };
 
 console.log(CheckIndex([1, 2, 3, 4, 57, 8, 9, 'Ajit'], 'Ajit'));
+
+//Fibonacci Series (0,1,1,2,3,5,8,13....) where keeping in array
+
+const Fibonacci = (num) => {
+  let currentValue = 0;
+  let nextValue = 1;
+  let nthvalue;
+  for (let i = 0; i < num; i++) {
+    console.log(currentValue);
+    nthvalue = currentValue + nextValue;
+    currentValue = nextValue;
+    nextValue = nthvalue;
+  }
+};
+console.log(Fibonacci(4));
